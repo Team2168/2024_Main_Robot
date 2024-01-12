620a5191d94c9cf1270976cbebe4e93ee04dfe4b
jonarbuckle229 <john.john36.3f@gmail.com>
2024-01-11 19:33:19 -0500

    finished led subsystem

--- src/main/java/org/team2168/Constants.java
+++ src/main/java/org/team2168/Constants.java
@@ -4,6 +4,8 @@
 
 package org.team2168;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,6 +15,13 @@ package org.team2168;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final class PneumaticsDevices {
+  public static final int RED_LIGHT = 6;
+  public static final int GREEN_LIGHT = 7;
+  public static final int BLUE_LIGHT = 8;
+
+    public static final PneumaticsModuleType MODULE_TYPE = PneumaticsModuleType.REVPH;
+  }
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
--- src/main/java/org/team2168/subsystems/LED.java
+++ src/main/java/org/team2168/subsystems/LED.java
@@ -4,6 +4,9 @@
 
 package org.team2168.subsystems;
 
+import org.team2168.Constants.PneumaticsDevices;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -13,7 +16,46 @@ public class LED extends SubsystemBase {
   private Solenoid greenLED;
   private Solenoid blueLED;
 
-  public LED() {}
+static LED instance = null;
+  public LED() {
+    redLED = new Solenoid(PneumaticsDevices.MODULE_TYPE, PneumaticsDevices.RED_LIGHT);
+    greenLED = new Solenoid(PneumaticsDevices.MODULE_TYPE, PneumaticsDevices.GREEN_LIGHT);
+    blueLED = new Solenoid(PneumaticsDevices.MODULE_TYPE, PneumaticsDevices.BLUE_LIGHT);
+  }
+
+  private void SetAllLEDs(boolean redIsOn, boolean greenIsOn, boolean blueIsOn) {
+redLED.set(redIsOn);
+greenLED.set(greenIsOn);
+blueLED.set(blueIsOn);
+  }
+  /** sets the red LED on and off
+ * @param isOn whether the red light should be on (true) or off (false)
+ */
+  public void redlight(boolean isOn) {
+    redLED.set(isOn);
+  }
+  /** sets the green LED on and off
+ *  @param isOn whether the green light should be on (true) or off (false)
+ */
+
+  public void greenlight(boolean isOn) {
+    greenLED.set(isOn);
+  }
+/** sets the blue LED on and off
+ * @param isOn whether the blue light should be on (true) or off (false)
+ */
+  public void bluelight(boolean isOn) {
+    blueLED.set(isOn);
+  }
+  
+
+public static LED getInstance() {
+  if (instance == null)
+  instance = new LED();
+  return instance;
+}
+
+ 
 
   @Override
   public void periodic() {
