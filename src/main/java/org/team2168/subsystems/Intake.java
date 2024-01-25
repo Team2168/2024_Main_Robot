77cbcb01d2f10e52687f613adc382a77d8b21bce
krlaferr <kferreiroc@gmail.com>
2024-01-24 19:49:16 -0500

    made the intake subsystem

--- src/main/java/org/team2168/subsystems/Intake.java
+++ src/main/java/org/team2168/subsystems/Intake.java
@@ -4,14 +4,100 @@
 
 package org.team2168.subsystems;
 
+import org.team2168.utils.TalonFXHelper;
+
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
 public class Intake extends SubsystemBase {
-  private static TalonFXHelper intakeRollerOne = new TalonFXHelper(0);
-  
-  /** Creates a new Intake. */
-  public Intake() {
+  private static TalonFX intakeRoller = new TalonFX(0);
+  private static Intake instance = null;
+  private static TalonFXConfiguration intakeRollerConfig = new TalonFXConfiguration();
+
+  public Intake getInstance() {
+    if(instance == null)
+    instance = new Intake();
+    return instance;
   }
+
+  private static InvertedValue intakeInvert = InvertedValue.Clockwise_Positive; //TODO: check value
+  
+  private static int intakeTimeoutMs = 30;
+  private static double peakOutput = 1.0;
+  private final boolean ENABLE_CURRENT_LIMIT = true;
+  private final double CONTINUOUS_CURRENT_LIMIT = 20.0;
+  private final double TRIGGER_THRESHOLD_LIMIT = 25;
+  private final double TRIGGER_THRESHOLD_TIME = 0.2;
+  private final double minuteInHundredMs = 600.0;
+  private static CurrentLimitsConfigs currentLimitConfig;
+  private double neutralDeadband = 0.05;
+  private double maxForwardOutput = 1;
+  private double maxBackwardOutput = -1;
+
+  private final double TICKS_PER_REV = 2048;
+  private final double GEAR_RATIO = 0; // TODO: Add later
+
+
+  /** Creates a new Intake. */
+  /*
+   * In intake constructor configures motor to have certain settings 
+   * Using the TalonFXConfig variable, gives it different values
+   * After putting all parameters into configuration variable, pushes it all into the intake motor
+   */
+  private Intake() {
+    intakeRoller.getConfigurator().apply( new TalonFXConfiguration()); //resets motor to its factory default
+    var motorConfigs = new MotorOutputConfigs();
+    var currentConfigs = new CurrentLimitsConfigs();
+
+    motorConfigs.withInverted(intakeInvert);
+    motorConfigs.withDutyCycleNeutralDeadband(neutralDeadband);
+
+    currentConfigs
+      .withSupplyCurrentLimitEnable(ENABLE_CURRENT_LIMIT)
+      .withSupplyCurrentLimit(CONTINUOUS_CURRENT_LIMIT)
+      .withSupplyCurrentThreshold(TRIGGER_THRESHOLD_LIMIT)
+      .withSupplyTimeThreshold(TRIGGER_THRESHOLD_TIME);
+    intakeRollerConfig.withCurrentLimits(currentLimitConfig);
+    //intakeRollerOneConfig.supplyCurrLimit = talonCurrentLimit;
+
+    var intakeRolle = intakeRoller.getConfigurator();
+    intakeRolle.apply(motorConfigs);
+    intakeRolle.apply(currentConfigs);
+    
+  }
+
+  /**
+   * sets the speed in percentage
+   * @param speed value is between -1.0 and 1.0
+   */
+  private static void setRollerSpeed(double speed) {
+    intakeRoller.set(speed);
+  }
+  
+  private double RPMToTicksPerOneHundredMS(double speedRPM) {
+    return (speedRPM/minuteInHundredMs) * (TICKS_PER_REV/GEAR_RATIO);
+  }
+
+  private double TicksPerOneHundredMSToRPM(double ticksPerHundredMs) {
+    return ticksPerHundredMs * (GEAR_RATIO/TICKS_PER_REV) * minuteInHundredMs;
+  }
+
+  
+
+  public double getSpeedRPM () {
+    return TicksPerOneHundredMSToRPM(intakeRoller.getVelocity().getValueAsDouble());
+  }
+
 
 
   @Override
