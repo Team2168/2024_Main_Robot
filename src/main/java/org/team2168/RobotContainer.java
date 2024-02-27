fbc18b86535b453c1810c36359cde233be96e9bd
krlaferr <kferreiroc@gmail.com>
2024-02-26 19:53:39 -0500

    added the indexer instance

--- src/main/java/org/team2168/RobotContainer.java
+++ src/main/java/org/team2168/RobotContainer.java
@@ -8,6 +8,8 @@ import org.team2168.Constants.OperatorConstants;
 import org.team2168.commands.Autos;
 import org.team2168.commands.ExampleCommand;
 import org.team2168.subsystems.ExampleSubsystem;
+import org.team2168.subsystems.Indexer;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -22,6 +24,9 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
+  private final Indexer indexer = Indexer.getInstance();
+
+
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
@@ -32,6 +37,7 @@ public class RobotContainer {
     configureBindings();
   }
 
+ 
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the
    * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
