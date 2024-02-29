dad5791bedb9530f5f11a00f2742b56219fba122
krlaferr <kferreiroc@gmail.com>
2024-02-28 19:40:09 -0500

    implemented logger

--- src/main/java/org/team2168/subsystems/Indexer.java
+++ src/main/java/org/team2168/subsystems/Indexer.java
@@ -22,6 +22,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.oblarg.oblog.annotations.Log;
 
 public class Indexer extends SubsystemBase {
   /** Creates a new Indexer. */  
