cfa60e0d8141234a59db9a52382abb5e6fe084fd
krlaferr <kferreiroc@gmail.com>
2024-03-02 13:05:54 -0500

    pivot testing

--- src/main/java/org/team2168/subsystems/IntakePivot.java
+++ src/main/java/org/team2168/subsystems/IntakePivot.java
@@ -48,22 +48,22 @@ public class IntakePivot extends SubsystemBase {
   private double neutralDeadband = 0.05;
   private double maxForwardOutput = 1;
   private double maxBackwardOutput = -1;
-  final double MIN_ANGLE = 0;
-  final double MAX_ANGLE = 120;
-  private double motionMagicAcceleration = 0.5;
-  private double motionMagicCruiseVelocity = 0.5;
+  final double MIN_ANGLE = -120;
+  final double MAX_ANGLE = 0;
+  private double motionMagicAcceleration = 2.0;
+  private double motionMagicCruiseVelocity = 2.0;
   private double kV = 0.12;
   private double kA = 0.1;
   private double sensorOffset = degreesToRot(-120);
   final MotionMagicTorqueCurrentFOC motionMagicTorqueCurrent = new MotionMagicTorqueCurrentFOC(0); // TODO: change maybe to sensor offset
 
   private final double TICKS_PER_REV = 2048;
-  private final static double GEAR_RATIO = 10.0/1.0;
+  private final static double GEAR_RATIO = 46.9;
 
   private double kP = 1.0;
   private double kI = 0;
   private double kD = 0;
-  private double kG = 0.05;
+  private double kG = -0.05;
   private GravityTypeValue gravityType = GravityTypeValue.Arm_Cosine;
 
 
